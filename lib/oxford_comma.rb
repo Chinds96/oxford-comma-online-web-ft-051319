eb436f4e4c0a1ac93f606387028cf0643e223860
Christopher Hinds <christopherhinds96@gmail.com>
2019-05-23 16:52:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+
+
+
+
+def oxford_comma(array)
+  array = []
+  if array.length < 2
+    return array
+  elsif array.length == 2 
+    array.join("and ")
+  elsif == 3 
 end
